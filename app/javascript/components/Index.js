408da3ef380fbff2f5ca51bd038eff0712b4e03e
Chris Kheng <chriskheng1609@gmail.com>
2020-02-13 21:44:20 +0800

    Install react bootstrap

---
+++ app/javascript/components/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Hello!</h1>
+            <Button>Hit me!</Button>
+        </div>
+    )
+};
+
+export default Index;

392f59891a1cca2115d49929dd0afab62b67eb18
Chris Kheng <chriskheng1609@gmail.com>
2020-02-14 21:20:27 +0800

    Test react-bootstrap modal

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const Index = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
     return (
         <div>
             <h1>Hello!</h1>
-            <Button>Hit me!</Button>
+            <Button type="button" onClick={() => setIsModalVisible(true)}>Hit me!</Button>
+            <Modal show={isModalVisible} onHide={() => setIsModalVisible(false)}>
+                Modal body!
+            </Modal>
         </div>
     )
 };

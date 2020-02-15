2298dc38f9aa7d0e2fd1d62ef6ff8858dbde214a
Chris Kheng <chriskheng1609@gmail.com>
2020-02-15 22:39:13 +0800

    Disable modal pop up at index page

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -1,17 +1,11 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 
 const Index = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-
     return (
         <div>
             <h1>Hello!</h1>
-            <Button type="button" onClick={() => setIsModalVisible(true)}>Hit me!</Button>
-            <Modal show={isModalVisible} onHide={() => setIsModalVisible(false)}>
-                Modal body!
-            </Modal>
+            <Button type="button">Hit me!</Button>
         </div>
     )
 };

c1767d704c682970998ebf2ab1f21f0d9a7cab59
Zhong Jun <zhongjun@jorked.com>
2020-02-16 01:52:23 +0800

    Store users and admins using devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions:           "admins/sessions"
+  }
+  devise_for :users, controllers: {
+    sessions:           "users/sessions",
+    passwords:          "users/passwords",
+    registrations:      "users/registrations",
+    confirmations:      "users/confirmations",
+  }
   root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

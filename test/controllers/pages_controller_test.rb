dd45544dd5060c31774a66a85ab3e7762021845d
Chris Kheng <chriskheng1609@gmail.com>
2020-02-15 14:13:56 +0800

    Fix failing test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get pages_home_url
+  test "should get index" do
+    get root_path
     assert_response :success
   end
 

4269ffa2e71b40cf117feb74c754872aa2b2caf2
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2020-02-16 16:45:22 +0800

    Store users and admins using devise gem (#5)

--- Gemfile
+++ Gemfile
@@ -54,3 +54,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "react-rails", "~> 2.6"
+
+# Authentication
+gem 'devise'
+gem 'rack-cors'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions:           "admins/sessions"
+  }
+  devise_for :users, controllers: {
+    sessions:           "users/sessions",
+    passwords:          "users/passwords",
+    registrations:      "users/registrations",
+    confirmations:      "users/confirmations",
+  }
   root 'pages#index'
   get 'signup', to: 'users#main'
   get 'login', to: 'users#main'

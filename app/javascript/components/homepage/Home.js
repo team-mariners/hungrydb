a3e27024d252b0599fa685f8fb416e1ca886db5c
lye-jw <lyejianwen@gmail.com>
2020-02-17 21:29:43 +0800

    Use React Routes in loading login and signup forms

--- app/javascript/components/homepage/Home.js
+++ app/javascript/components/homepage/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import NaviBar from './NaviBar';
+import LoginForm from '../users/main/LoginForm';
+import SignUpForm from '../users/main/SignUpForm';
 
-const Home = () => {
+export default function Home() {
     return (
         <Router>
             <NaviBar/>
+            <Route path="/login" render={() => <LoginForm/>}/>
+            <Route path="/signup" render={() => <SignUpForm/>}/>
         </Router>
     )
 };
-
-export default Home;
--- app/javascript/components/homepage/NaviBar.js
+++ app/javascript/components/homepage/NaviBar.js
@@ -8,8 +8,11 @@ const NaviBar = () => {
             <Navbar className="homepage-navbar" variant="dark">
                 <Navbar.Brand href="/">HungryDB</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Navbar.Text style={{color: 'yellow', paddingLeft:'300px', fontSize: '18px'}}>WELCOME!</Navbar.Text>
+                    <Navbar.Text style={{color: 'yellow', paddingLeft:'300px', fontSize: '18px'}}>
+                        WELCOME!
+                    </Navbar.Text>
                 </Nav>
+
                 <Nav className="justify-content-end">
                     <Nav.Link href="/login" style={{color: 'white'}}>Log In</Nav.Link>
                     <Nav.Link href="/signup"style={{color: 'white'}}>Sign Up</Nav.Link>

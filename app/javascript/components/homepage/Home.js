c15060b1b5483bf0e49a67f5f2714a19fa7dccab
lye-jw <lyejianwen@gmail.com>
2020-02-17 22:02:51 +0800

    Fix assets pipeline CI issue

--- app/javascript/components/homepage/Home.js
+++ app/javascript/components/homepage/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import NaviBar from './NaviBar';
+// import HomeContainer from './HomeContainer';
 import LoginForm from '../users/main/LoginForm';
 import SignUpForm from '../users/main/SignUpForm';
 
@@ -9,6 +10,7 @@ export default function Home() {
     return (
         <Router>
             <NaviBar/>
+            {/* <HomeContainer/> */}
             <Route path="/login" render={() => <LoginForm/>}/>
             <Route path="/signup" render={() => <SignUpForm/>}/>
         </Router>

a5043a276d125905807dbb6db4adf85a626514d6
Chris Kheng <chriskheng1609@gmail.com>
2020-02-19 19:47:52 +0800

    Refactor login form into formik component

--- app/javascript/components/users/main/LoginForm.js
+++ app/javascript/components/users/main/LoginForm.js
@@ -1,28 +1,65 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 
 const LoginForm = () => {
-    const handleLogin = () => {
-        event.preventDefault();
+    const schema = yup.object({
+        email: yup.string().required(),
+        password: yup.string().required()
+    });
+
+    const formInitialValues = {
+        email: '',
+        password: ''
+    }
+
+    const handleLogin = (values) => {
+        console.log(values);
     };
 
     return (
-        <Form>
-            <Form.Group>
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email"/>
-            </Form.Group>
+        <Formik
+            validationSchema={schema}
+            initialValues={formInitialValues}
+            onSubmit={handleLogin}
+        >
+            {({
+                handleSubmit,
+                handleChange,
+                values,
+                errors,
+                touched
+            }) => (
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        isInvalid={touched.email && !!errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">Email cannot be empty.</Form.Control.Feedback>
+                </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password"/>
-            </Form.Group>
-
-            <Button variant="primary" type="submit" onClick={handleLogin}>
-                Login
-            </Button>
-        </Form>
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        isInvalid={touched.password && !!errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">Password is invalid.</Form.Control.Feedback>
+                </Form.Group>
+                <Button variant="primary" type="submit">Login</Button>
+            </Form>
+           )}
+        </Formik>
     )
 };
 

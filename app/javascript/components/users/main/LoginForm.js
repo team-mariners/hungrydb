35c2ac90de8573936f372b643cd5cb0324114700
Chris Kheng <42091735+ChrisKheng@users.noreply.github.com>
2020-02-20 00:12:35 +0800

    Refactor sign up and login form into Formik component (#7)

--- app/javascript/components/users/main/LoginForm.js
+++ app/javascript/components/users/main/LoginForm.js
@@ -1,28 +1,69 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 
 const LoginForm = () => {
-    const handleLogin = () => {
-        event.preventDefault();
+    /*
+     * Identifier refers to username or email.
+     * The validation rules of username and email is the same as the ones in signup form.
+     */
+    const schema = yup.object({
+        identifier: yup.string().matches(/^\w+$|^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/).required(),
+        password: yup.string().required()
+    });
+
+    const formInitialValues = {
+        identifier: '',
+        password: ''
+    };
+
+    const handleLogin = (values) => {
+        console.log(values);
     };
 
     return (
-        <Form>
-            <Form.Group>
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email"/>
-            </Form.Group>
+        <Formik
+            validationSchema={schema}
+            initialValues={formInitialValues}
+            onSubmit={handleLogin}
+        >
+            {({
+                handleSubmit,
+                handleChange,
+                values,
+                errors,
+                touched
+            }) => (
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Username or Email</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="identifier"
+                        value={values.identifier}
+                        onChange={handleChange}
+                        isInvalid={touched.identifier && !!errors.identifier}
+                    />
+                    <Form.Control.Feedback type="invalid">Username or email is invalid.</Form.Control.Feedback>
+                </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password"/>
-            </Form.Group>
-
-            <Button variant="primary" type="submit" onClick={handleLogin}>
-                Login
-            </Button>
-        </Form>
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        isInvalid={touched.password && !!errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">Password is invalid.</Form.Control.Feedback>
+                </Form.Group>
+                <Button variant="primary" type="submit">Login</Button>
+            </Form>
+           )}
+        </Formik>
     )
 };
 

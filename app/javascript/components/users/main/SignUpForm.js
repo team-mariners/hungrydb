066e98982e3461722f53a05f6706105fd30a6cc7
Chris Kheng <chriskheng1609@gmail.com>
2020-02-19 19:24:00 +0800

    Refactor sign up form into formik component

--- app/javascript/components/users/main/SignUpForm.js
+++ app/javascript/components/users/main/SignUpForm.js
@@ -2,68 +2,121 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 
 const SignUpForm = () => {
-    const [isAgreed, setIsAgreed] = useState(false);
-    const [isInvalidSubmission, setIsInvalidSubmission] = useState(false);
+    // Using yup helps us to declaratively verify the input data
+    const schema = yup.object({
+        username: yup.string().required(),
+        email: yup.string().required(),
+        password: yup.string().required(),
+        confirmPassword: yup.string().required(),
+        terms: yup.bool().oneOf([true])
+    });
 
-    const handleSignUp = (event) => {
-        event.preventDefault();
+    const formInitialValues = {
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        terms: false
+    }
 
-        if (!isAgreed) {
-            setIsInvalidSubmission(true);
-            return;
-        }
-
-        setIsInvalidSubmission(false);
+    const handleSignUp = (values) => {
+        console.log(values);
     };
 
     return (
-        <Form>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text"/>
-            </Form.Group>            
+        <Formik
+            validationSchema={schema}
+            onSubmit={handleSignUp}
+            initialValues={formInitialValues}
+        >
+            {({
+                handleSubmit,
+                handleChange,
+                values,
+                errors,
+                touched
+            }) => (
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="username"
+                        value={values.username}
+                        onChange={handleChange}
+                        isInvalid={touched.username && !!errors.username}
+                        />
+                    <Form.Control.Feedback type="invalid">Username cannot be empty.</Form.Control.Feedback>
+                </Form.Group>
 
-            <Form.Group controlId="formEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email"/>
-            </Form.Group>
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        isInvalid={touched.email && !!errors.email}
+                        />
+                    <Form.Control.Feedback type="invalid">Email cannot be empty.</Form.Control.Feedback>
+                </Form.Group>
 
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password"/>
-            </Form.Group>
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        isInvalid={touched.password && !!errors.password}
+                        />
+                    <Form.Control.Feedback type="invalid">Password is invalid.</Form.Control.Feedback>
+                </Form.Group>
 
-            <Form.Group controlId="formConfirmPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password"/>
-            </Form.Group>
+                <Form.Group controlId="formConfirmPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="confirmPassword"
+                        value={values.confirmPassword}
+                        onChange={handleChange}
+                        isInvalid={touched.confirmPassword && !!errors.confirmPassword}
+                        />
+                    <Form.Control.Feedback type="invalid">Password does not match.</Form.Control.Feedback>
+                </Form.Group>
 
-            <Form.Group controlId="formUserType">
-                <Form.Label>Sign up as</Form.Label>
-                <Form.Control as="select">
-                    <option>Customer</option>
-                    <option>Rider</option>
-                </Form.Control>
-            </Form.Group>
+                <Form.Group controlId="formUserType">
+                    <Form.Label>Sign up as</Form.Label>
+                    <Form.Control as="select">
+                        <option>Customer</option>
+                        <option>Rider</option>
+                    </Form.Control>
+                </Form.Group>
 
-            <Form.Group controlId="formTerms">
-                <Form.Check>
-                    <Form.Check.Input type="checkbox"
-                        isInvalid={isInvalidSubmission}
-                        onClick={() => setIsAgreed(!isAgreed)}/>
-                    <Form.Check.Label>I agree to the terms and conditions.</Form.Check.Label>
-                    <FormControl.Feedback type="invalid">
-                        You need to agree to the terms and conditions first.
+                <Form.Group controlId="formTerms">
+                    <Form.Check>
+                        <Form.Check.Input type="checkbox"
+                            name="terms"
+                            value={values.terms}
+                            isInvalid={touched.terms && !!errors.terms}
+                            onChange={handleChange}/>
+                        <Form.Check.Label>I agree to the terms and conditions.</Form.Check.Label>
+                        <FormControl.Feedback type="invalid">
+                            You need to agree to the terms and conditions first.
                     </FormControl.Feedback>
-                </Form.Check>
-            </Form.Group>
+                    </Form.Check>
+                </Form.Group>
 
-            <Button variant="primary" type="submit" onClick={handleSignUp}>
-                Sign up!
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                    Sign up!
             </Button>
-        </Form>
+            </Form>
+            )}
+        </Formik>
     )
 };
 

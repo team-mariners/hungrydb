7e2d08999c74750e841456fc24e6383709236e00
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2020-02-22 10:11:30 +0800

    Add seed users (#11)
    * Add seed users
    
    * Remove Admin model
    
    * Delete unnecessary files for Admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Test customer
+User.create(
+    username: 'customer',
+    password: '12345678',
+    email: 'customer@example.com',
+    roles: 'customer'
+)
+
+# Test rider
+User.create(
+    username: 'rider',
+    password: '12345678',
+    email: 'rider@example.com',
+    roles: 'customer,rider'
+)
+
+# Test restaurant manager
+User.create(
+    username: 'manager',
+    password: '12345678',
+    email: 'manager@example.com',
+    roles: 'customer,manager'
+)
+
+# Test administrator
+User.create(
+    username: 'admin',
+    password: '12345678',
+    email: 'admin@example.com',
+    roles: 'customer,admin'
+)

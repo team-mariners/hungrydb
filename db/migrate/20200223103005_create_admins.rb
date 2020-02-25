c7d9cd8c1352493c7bfa8d60337b556294ae08f8
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2020-02-25 23:48:11 +0800

    Fix model definition to allow storing of role attributes (#21)

--- db/migrate/20200223103005_create_admins.rb
+++ db/migrate/20200223103005_create_admins.rb
@@ -1,7 +1,7 @@
 class CreateAdmins < ActiveRecord::Migration[6.0]
   def change
     create_table :admins do |t|
-      t.references :users, null: false, foreign_key: true
+      t.belongs_to :user, index: { unique: true }, foreign_key: "id"
 
       t.timestamps
     end

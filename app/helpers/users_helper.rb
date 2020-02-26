df4dc81366b3eb314c6cfc0f2e2de64ec142cff9
Chris Kheng <chriskheng1609@gmail.com>
2020-02-26 11:05:05 +0800

    Configure user object given to Manger index page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,20 @@
 module UsersHelper
     protected def has_role(role)
-        if !user_signed_in?
-            return false
-        end
+      if !user_signed_in?
+        return false
+      end
 
-        rolelist = current_user.roles.split(',')
+      rolelist = current_user.roles.split(',')
 
-        if rolelist.include?(role)
-            return true
-        else
-            return false
-        end
+      if rolelist.include?(role)
+        return true
+      else
+        return false
+      end
+    end
+
+    # Returns a customised user which only consists of attributes that are chosen to be exposed.
+    def get_user_hash(user)
+      userHash = {id: user.id, username: user.username, email: user.email, roles: user.roles}
     end
 end
--- app/javascript/components/managers/Index.js
+++ app/javascript/components/managers/Index.js
@@ -8,16 +8,17 @@ import Stats from './Stats';
 import Reviews from './Reviews';
 
 const Index = (props) => {
+    console.log(props);
     return (
         <Router>
             <NavigationBar/>
-            <Route exact path="/" render={() => <Dashboard  currentUser={props.currentUser}/>}/>
+            <Route exact path="/" render={() => <Dashboard  currentUser={props.currentManager}/>}/>
             <Route exact path="/manager/orders" render={() => <Orders/>}/>
             <Route exact path="/manager/stats" render={() => <Stats/>}/>
             <Route exact path="/manager/reviews" render={() => <Reviews/>}/>
             <Route exact path="/manager/manage" render={() => <Manage/>}/>
         </Router>
     )
-}
+};
 
 export default Index;

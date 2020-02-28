cb8be326442f28fe03a4940a38b6b8e284d00b58
Chris Kheng <chriskheng1609@gmail.com>
2020-02-28 13:38:01 +0800

    Change position of notification

--- app/javascript/components/managers/ManageMenu.js
+++ app/javascript/components/managers/ManageMenu.js
@@ -1,19 +1,12 @@
 import React, { useState } from 'react';
 import ToolBar from './manageMenu/ToolBar';
-import Modal from '../utilities/Modal';
-import Button from 'react-bootstrap/Button';
 
 const ManageMenu = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
 
     return (
         <div className="p-3">
             <h1>Menu</h1>
             <ToolBar />
-            <Button type="button" onClick={() => setIsModalVisible(true)}>Hit me!</Button>
-            <Modal show={isModalVisible} onClose={() => setIsModalVisible(false)}>
-                <h1>Hello!</h1>
-            </Modal>
         </div>
     )
 };

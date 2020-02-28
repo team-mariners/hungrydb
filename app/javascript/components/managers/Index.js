d4578880b52544063581fd4d8d76528fdbe5b13e
Chris Kheng <chriskheng1609@gmail.com>
2020-02-28 10:57:09 +0800

    Create restaurants table and develop manager's manage menu page (#24)
    * Update restaurant model and schema
    
    * Fix sign up and login fail bug
    
    * Configure user object given to Manger index page
    
    * Modify manager's pages
    
    * Set up basic tool bar for manage menu page
    
    * Change find_by_sql to find_by in managers_helper.rb
    
    * Fix merge conflict issues
    
    Co-authored-by: Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>

--- app/javascript/components/managers/Index.js
+++ app/javascript/components/managers/Index.js
@@ -6,18 +6,24 @@ import Manage from './Manage';
 import Orders from './Orders';
 import Stats from './Stats';
 import Reviews from './Reviews';
+import ManageMenu from './ManageMenu';
+import ManagePromo from './ManagePromo';
+import ManageInfo from './ManageInfo';
 
 const Index = (props) => {
+    console.log(props);
     return (
         <Router>
             <NavigationBar/>
-            <Route exact path="/" render={() => <Dashboard  currentUser={props.currentUser}/>}/>
+            <Route exact path="/" render={() => <Dashboard  currentUser={props.info}/>}/>
             <Route exact path="/manager/orders" render={() => <Orders/>}/>
             <Route exact path="/manager/stats" render={() => <Stats/>}/>
             <Route exact path="/manager/reviews" render={() => <Reviews/>}/>
-            <Route exact path="/manager/manage" render={() => <Manage/>}/>
+            <Route exact path="/manager/manage_menu" render={() => <ManageMenu/>}/>
+            <Route exact path="/manager/manage_promo" render={() => <ManagePromo/>}/>
+            <Route exact path="/manager/manage_info" render={() => <ManageInfo/>}/>
         </Router>
     )
-}
+};
 
 export default Index;
---
+++ app/javascript/components/managers/ManageMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToolBar from './manageMenu/ToolBar';
+
+const ManageMenu = () => {
+    return (
+        <div className="p-3">
+            <h1>Menu</h1>
+            <ToolBar/>
+        </div>
+    )
+};
+
+export default ManageMenu;

14e7eace688cb4454a37bf1d1ffea7ea7201cee2
Chris Kheng <chriskheng1609@gmail.com>
2020-02-28 13:36:45 +0800

    Add custom modal

--- app/javascript/components/managers/ManageMenu.js
+++ app/javascript/components/managers/ManageMenu.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToolBar from './manageMenu/ToolBar';
+import Modal from '../utilities/Modal';
+import Button from 'react-bootstrap/Button';
 
 const ManageMenu = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
     return (
         <div className="p-3">
             <h1>Menu</h1>
-            <ToolBar/>
+            <ToolBar />
+            <Button type="button" onClick={() => setIsModalVisible(true)}>Hit me!</Button>
+            <Modal show={isModalVisible} onClose={() => setIsModalVisible(false)}>
+                <h1>Hello!</h1>
+            </Modal>
         </div>
     )
 };

8d22df8fc54cda61a9061c7777aea7a7df181a6b
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2020-03-08 10:09:05 +0800

    Add pages for admins (#32)

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,44 @@
 module AdminsHelper
+    def get_statistics
+        output = {
+            numUsers: get_users_count,
+            numAdmins: get_admins_count,
+            numManagers: get_managers_count,
+            numRiders: get_riders_count,
+            numCustomers: get_customers_count,
+            numRestaurants: get_restaurants_count,
+            numFood: get_food_count
+        }
+
+        return output
+    end
+
+    protected
+    def get_users_count
+        return User.all.count
+    end
+
+    def get_admins_count
+        return Admin.all.count
+    end
+
+    def get_managers_count
+        return Manager.all.count
+    end
+
+    def get_riders_count
+        return Rider.all.count
+    end
+
+    def get_customers_count
+        return Customer.all.count
+    end
+
+    def get_restaurants_count
+        return Restaurant.all.count
+    end
+
+    def get_food_count
+        return Food.all.count
+    end
 end

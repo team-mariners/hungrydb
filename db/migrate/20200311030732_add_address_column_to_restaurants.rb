8bb5c4052af93a354a2d284b46edc85a2f8071ab
Chris Kheng <chriskheng1609@gmail.com>
2020-03-11 11:17:39 +0800

    Add address column to restaurants

---
+++ db/migrate/20200311030732_add_address_column_to_restaurants.rb
@@ -0,0 +1,5 @@
+class AddAddressColumnToRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    add_column :restaurants, :address, :string 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_121654) do
+ActiveRecord::Schema.define(version: 2020_03_11_030732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_03_08_121654) do
     t.decimal "minOrderCost", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
     t.index ["manager_id"], name: "index_restaurants_on_manager_id", unique: true
   end
 

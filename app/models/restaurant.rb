91be2aee26e753b855eb278ee5eee610706b5ca1
Chris Kheng <chriskheng1609@gmail.com>
2020-03-11 11:26:02 +0800

    Update restaurant minOrderCost verification

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
     belongs_to :manager 
     has_many :foods, validate: true, dependent: :destroy
     has_many :food_categories, validate: true, dependent: :destroy
-    validates :minOrderCost, numericality: { greater_than_or_equal_to: 0 }
+    validates :minOrderCost, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than_or_equal_to: 0 }
 end

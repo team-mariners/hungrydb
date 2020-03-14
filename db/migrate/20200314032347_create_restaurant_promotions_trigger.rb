54db95d284b6b7d1ee9711ba2a9d61c566dc301f
Chris Kheng <chriskheng1609@gmail.com>
2020-03-14 15:56:43 +0800

    Set up tables and triggers for promotion feature

---
+++ db/migrate/20200314032347_create_restaurant_promotions_trigger.rb
@@ -0,0 +1,34 @@
+class CreateRestaurantPromotionsTrigger < ActiveRecord::Migration[6.0]
+  def up 
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION check_has_promotions_exist() RETURNS TRIGGER AS $$
+      DECLARE 
+        exist integer; 
+      BEGIN
+        SELECT DISTINCT 1 INTO exist
+          FROM has_promotions
+          WHERE restaurant_promotion_id = NEW.promotion_id;
+        IF exist IS NULL THEN
+          RAISE exception 'Must insert a tuple into has_promotions upon inserting into restaurant_promotions';
+        END IF;
+        RETURN NULL;
+      END;
+      $$ LANGUAGE plpgsql;
+
+      DROP TRIGGER IF EXISTS restaurant_promotion_trigger ON restaurant_promotions CASCADE;
+      CREATE CONSTRAINT TRIGGER restaurant_promotion_trigger
+        AFTER INSERT
+        ON restaurant_promotions
+        DEFERRABLE INITIALLY DEFERRED
+        FOR EACH ROW
+        EXECUTE FUNCTION check_has_promotions_exist();
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP TRIGGER restaurant_promotion_trigger ON restaurant_promotions CASCADE;
+      DROP FUNCTION check_has_promotions_exist() CASCADE;      
+    SQL
+  end
+end

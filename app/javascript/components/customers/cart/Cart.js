e24f2c1058a832c18c0963c0db526c55d1f4effd
lye-jw <lyejianwen@gmail.com>
2020-03-23 00:04:41 +0800

    Add validation to prevent using repeated promo codes

--- app/javascript/components/customers/cart/Cart.js
+++ app/javascript/components/customers/cart/Cart.js
@@ -12,24 +12,25 @@ class Cart extends React.Component {
         console.log(this.props.orders);
         console.log(sessionStorage.getItem('orders'));
         this.orders = this.props.orders === null
-                    ? JSON.parse(sessionStorage.getItem('orders'))
-                    : this.props.orders;
-        
+            ? JSON.parse(sessionStorage.getItem('orders'))
+            : this.props.orders;
+
         this.handlePromoChange = this.handlePromoChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.state = {entered_promotion: "", promotions: null};
+        this.state = { entered_promo: "", promotions: null };
 
         this.amountDue = 0;
         this.discountPercentage = sessionStorage.getItem("discount_percent");
+        this.usedPromos = sessionStorage.getItem("used_promos");
     }
 
     componentDidMount() {
         axios.get('/api/v1/promotions/' + sessionStorage.getItem('restaurant_id')
-                    + '/promotions.json')
+            + '/promotions.json')
             .then(
                 (response) => {
                     let retrieved_promotions = response.data;
-                    this.setState({promotions: retrieved_promotions});
+                    this.setState({ promotions: retrieved_promotions });
                     console.log(this.state.promotions);
                 })
             .catch(error => {
@@ -43,21 +44,35 @@ class Cart extends React.Component {
         // Set amountDue to 0 for re-render after setState
         this.amountDue = 0;
         console.log(e.target.value);
-        this.setState({entered_promotion: e.target.value});
+        this.setState({ entered_promo: e.target.value });
     }
 
     handleSubmit(e) {
+        // Check if promo used before
+        if (this.usedPromos) {
+            let usedPromosArray = this.usedPromos.split(" ");
+            for (let usedCode of usedPromosArray) {
+                if (usedCode == this.state.entered_promo) {
+                    alert("You have already used " + usedCode);
+                    return;
+                }
+            }
+        }
+
+        // Apply promo, save total discount % & used promo to sessionStorage
         let promotionsList = this.state.promotions;
-        console.log("HEEERE");
         console.log(promotionsList);
-        // Apply discount, create negative amountDue for refresh render
         for (let promo of promotionsList) {
-            if (promo.promocode === this.state.entered_promotion) {
+            if (promo.promocode === this.state.entered_promo) {
                 let currPercentage = this.discountPercentage == null
-                                    ? 0
-                                    : parseFloat(this.discountPercentage);
+                    ? 0
+                    : parseFloat(this.discountPercentage);
                 let newPercentage = currPercentage + parseFloat(promo.percentage) / 100;
-                sessionStorage.setItem('discount_percent', newPercentage.toFixed(2));
+                sessionStorage.setItem("discount_percent", newPercentage.toFixed(2));
+                let currUsedPromos = this.usedPromos == null
+                    ? ""
+                    : this.usedPromos;
+                sessionStorage.setItem("used_promos", currUsedPromos + " " + promo.promocode);
                 alert(promo.promocode + " applied for " + promo.percentage + "% off!" + currPercentage);
                 return;
             }
@@ -65,56 +80,56 @@ class Cart extends React.Component {
     }
 
     render() {
-    if (this.orders === null) {
-        return <h3>Your cart is empty.</h3>
-    } else {
-        let cart = [];
-        for (let item in this.orders) {
-            if (this.orders.hasOwnProperty(item)) {
-                let foodDetails = this.orders[item];
-                cart.push(
-                    <CartItem foodName={item} foodDetails={foodDetails} />
-                )
-                this.amountDue += foodDetails.price * foodDetails.quantity;
+        if (this.orders === null) {
+            return <h3>Your cart is empty.</h3>
+        } else {
+            let cart = [];
+            for (let item in this.orders) {
+                if (this.orders.hasOwnProperty(item)) {
+                    let foodDetails = this.orders[item];
+                    cart.push(
+                        <CartItem foodName={item} foodDetails={foodDetails} />
+                    )
+                    this.amountDue += foodDetails.price * foodDetails.quantity;
+                }
             }
+            return (
+                <div className='cart-container'>
+                    <div><br /></div>
+                    <h3>Ordering From: {sessionStorage.getItem('restaurant_name')}</h3>
+                    <div><br /></div>
+                    <Table responsive className='cart-table'>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Food</th>
+                                <th>Single Price</th>
+                                <th>Quantity</th>
+                                <th>Total Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cart}
+                        </tbody>
+                    </Table>
+                    <div><br /></div>
+
+                    <Form inline onSubmit={this.handleSubmit}>
+                        <FormControl type="text"
+                            placeholder="Promo Code"
+                            className="mr-sm-2" style={{ width: 300 }}
+                            onChange={this.handlePromoChange} />
+                        <Button type="submit" variant="success">Apply</Button>
+                    </Form>
+                    <div><br /></div>
+
+                    <h3 className='cart-amount-due'>
+                        Amount Due: ${(this.amountDue - this.amountDue * this.discountPercentage).toFixed(2)}
+                    </h3>
+                    <div><br /></div>
+                </div>
+            )
         }
-        return (
-            <div className='cart-container'>
-                <div><br /></div>
-                <h3>Ordering From: {sessionStorage.getItem('restaurant')}</h3>
-                <div><br /></div>
-                <Table responsive className='cart-table'>
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Food</th>
-                            <th>Single Price</th>
-                            <th>Quantity</th>
-                            <th>Total Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {cart}
-                    </tbody>
-                </Table>
-                <div><br /></div>
-
-                <Form inline onSubmit={this.handleSubmit}>
-                    <FormControl type="text"
-                        placeholder="Promo Code"
-                        className="mr-sm-2" style={{ width: 300 }}
-                        onChange={this.handlePromoChange} />
-                    <Button type="submit" variant="success">Apply</Button>
-                </Form>
-                <div><br /></div>
-
-                <h3 className='cart-amount-due'>
-                    Amount Due: ${(this.amountDue - this.amountDue * this.discountPercentage).toFixed(2)}
-                </h3>
-                <div><br /></div>
-            </div>
-        )
-    }
     }
 }
 

55150e7123f574fefd6eb0d435fd159deaedac0e
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-03-23 11:19:21 +0800

    added Rider table sql

--- db/migrate/20200223103255_create_riders.rb
+++ db/migrate/20200223103255_create_riders.rb
@@ -11,4 +11,24 @@ class CreateRiders < ActiveRecord::Migration[6.0]
       t.timestamps
     end
   end
+
+  def up
+    execute "CREATE TABLE riders (
+      id bigint NOT NULL,
+      currLocation varchar(300) NOT NULL,
+      status varchar(300) NOT NULL,
+      comission numeric NOT NULL,
+      PRIMARY KEY(id),
+      FOREIGN KEY(id) REFERENCES users
+        ON DELETE CASCADE
+    );"
+
+    # This will auto drop the sequence when Riders is dropped
+    execute "ALTER SEQUENCE riders_id_seq OWNED BY riders.id;"
+  end
+
+  # For rolling back
+  def down
+    execute "DROP TABLE riders;"
+  end
 end

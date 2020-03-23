3a9f4a95bd4541f4d26a5748c14931714363ed3c
Chris Kheng <chriskheng1609@gmail.com>
2020-03-23 09:06:22 +0800

    [Partial] Set up backend for view orders manager page

--- app/controllers/api/v1/orders/orders_controller.rb
+++ app/controllers/api/v1/orders/orders_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Api::V1::Orders::OrdersController < Api::V1::BaseController
+  before_action :get_restaurant, only: %i[index_restaurant]
+
   def index
     orders_array = []
     orders = helpers.retrieve_orders().to_a
@@ -16,5 +18,66 @@ class Api::V1::Orders::OrdersController < Api::V1::BaseController
     end
 
     render json: { 'orders': orders_array }
+  end
+
+  def index_restaurant
+    orders = ActiveRecord::Base.connection.exec_query(
+      "SELECT oid, point_offset, payment_method, delivery_fee, date_time, status, food_review,
+        (SELECT username FROM Users WHERE id = Orders.customer_id) AS customer_name,
+        (SELECT percentage FROM Promotions WHERE id = Orders.promo_id) AS promo_percentage
+      FROM Orders LEFT OUTER JOIN Reviews USING (oid) 
+      WHERE restaurant_id = #{@restaurant.id}
+      ORDER BY oid;"
+    ).to_a    
+
+    order_ids = get_order_ids(orders)
+
+    ordered_foods = ActiveRecord::Base.connection.exec_query(
+      "WITH T AS (
+        SELECT *
+        FROM Comprises
+        WHERE oid IN (#{array.join(",")})
+      )    
+      SELECT *
+      FROM T JOIN Foods  ON (T.food_id = Foods.id)
+      ORDER BY T.oid;"      
+    ).to_a  
+  end
+
+  private
+  def get_restaurant
+    @restaurant = helpers.get_restaurant(current_user)
+  end
+
+  def get_order_ids
+    result = []
+    orders.each do |order|
+      result.push(order["oid"])
+    end
+    return result
+  end
+
+  def process_orders(orders, ordered_foods)
+    index = 0
+    array = []
+    
+    for food in ordered_foods
+      if food["oid"] == orders[index]["oid"]
+        array << food
+      else 
+        orders[index]["foods"] = array        
+        index += 1        
+        array = []
+      end
+    end
+
+    # Handle the last append
+    orders[index]["foods"] = array  
+  end
+
+  def get_order(row)
+    result = row.clone
+    return result.extract!("oid", "customer_id", "promo_id", "point_offset", "payment_method",
+      "delivery_fee", "date_time", "status")
   end
 end

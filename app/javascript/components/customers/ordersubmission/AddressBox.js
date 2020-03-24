d7eb5d806a4f17e06549cdbb4e86aa21a4c1b354
lye-jw <lyejianwen@gmail.com>
2020-03-24 22:37:16 +0800

    Implement selection of 5 recent addresses when placing order

--- app/javascript/components/customers/ordersubmission/AddressBox.js
+++ app/javascript/components/customers/ordersubmission/AddressBox.js
@@ -2,17 +2,62 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import axios from 'axios';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 
-const AddressBox = (props) => {
 
-    return (
-        <Form inline onSubmit={props.handleSubmit}>
-        <FormControl type="text"
-            placeholder="Address"
-            className="mr-sm-2" style={{ width: 1000 }}
-            onChange={props.handleInsertChange} />
-    </Form>
-    )
+class AddressBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { recentAddresses: [] };
+    }
+
+    componentDidMount() {
+        axios.get('/api/v1/orders/orders.json')
+            .then(
+                (response) => {
+                    const retrieved_orders = response.data.orders;
+                    let orders = retrieved_orders;
+                    console.log(orders);
+
+                    // Addresses already in order thanks to SQL Order By
+                    let addresses = orders.map(order => order["address"])
+
+                    // Remove duplicates
+                    let uniqueAddresses = addresses.filter((address, index) =>
+                        addresses.indexOf(address) == index
+                    )
+                    /* Using Set:
+                    let uniqueAddresses = [...new Set(addresses)]
+                    OR uniqueAddresses = Array.from(new Set(addresses))
+                    */
+
+                    this.setState({ recentAddresses: uniqueAddresses.slice(0, 1) });
+                    console.log(this.state.recentAddresses);
+                })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        return (
+            <Typeahead
+                id="basic-example"
+                // onChange={selected => this.setState({ selected })}
+                options={this.state.recentAddresses}
+                placeholder="Choose a state..."
+            />
+
+
+            // <Form inline>
+            //     <FormControl type="text"
+            //         placeholder="Address"
+            //         className="mr-sm-2" style={{ width: 1000 }}
+            //         onChange={props.handleInsertChange} />
+            // </Form>
+        )
+    }
 }
 
 export default AddressBox;

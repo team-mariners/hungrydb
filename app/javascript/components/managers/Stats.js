acbda4bdbc1f6cd704fc63fa0172323667a34ab9
Chris Kheng <chriskheng1609@gmail.com>
2020-03-24 23:49:46 +0800

    Add feature to select month for summary

--- app/javascript/components/managers/Stats.js
+++ app/javascript/components/managers/Stats.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import DatePicker from './summary/DatePicker';
+import React, { useState } from 'react';
+import MonthPicker from './summary/MonthPicker';
 import MonthSummary from './summary/MonthSummary';
+import moment from 'moment-timezone';
 
 const Stats = () => {
+    const [month, setMonth] = useState(moment());
+    const MONTH_FORMAT = "MMMM, YYYY";
+
     return (
         <div className="p-3">
-            <h1>March, 2020</h1>                        
-            <DatePicker className="my-3"/>
+            <h1>{month.format(MONTH_FORMAT)}</h1>                        
+            <MonthPicker
+                className="my-3"
+                month={month}
+                setMonth={setMonth}/>
             <MonthSummary/>
         </div>
     )

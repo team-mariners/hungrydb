7e4e9ea1cf27d056957ff5ddd118ab0966101059
lye-jw <lyejianwen@gmail.com>
2020-03-31 16:44:59 +0800

    Add trigger to update Customer points after placing Order

--- app/javascript/components/customers/ordersubmission/CompleteOrder.js
+++ app/javascript/components/customers/ordersubmission/CompleteOrder.js
@@ -62,7 +62,7 @@ class CompleteOrder extends React.Component {
                                 : 0;
         order["payment_method"] = this.state.paymentMethod;
         order["delivery_fee"] = 3.00
-        order["total_price"] = sessionStorage.getItem('amount_due');
+        order["total_price"] = parseInt(sessionStorage.getItem('amount_due'));
         order["status"] = "in progress";
         order["foods"] = JSON.parse(sessionStorage.getItem('foods'));
         order["customer_location"] = this.state.address;

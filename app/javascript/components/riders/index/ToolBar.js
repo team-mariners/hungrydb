211f26cf43047afa08e7faef3c01abcf809d5d1e
Chris Kheng <chriskheng1609@gmail.com>
2020-04-01 13:42:35 +0800

    Refactor data types in working_intervals table (#104)
    * Set up rider home page
    
    * Add basic clock in and out system in rider's dashboard
    
    * Refactor front end of clock in and out system
    
    * Add basic querying of working intervals for clocking out

--- app/javascript/components/riders/index/ToolBar.js
+++ app/javascript/components/riders/index/ToolBar.js
@@ -1,46 +1,46 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import ClockIn from './ClockIn';
+import ClockOut from './ClockOut';
+import moment from 'moment-timezone';
 
 const ToolBar = (props) => {
-    const clockIn = () => {
-        axios.post("/rider/clock_in")
-            .then(result => {                
-                const newClockInData = {...props.clockedInData};
-                newClockInData.clock_in = result.data.clock_in;
-                props.setClockedInData(newClockInData);
-            }).catch(error => {
-                console.log(error);
-            });    
-    }
+    const [isShowClockIn, showClockIn] = useState(false);
+    const [isShowClockOut, showClockOut] = useState(false);
 
-    const clockOut = () => {
-        axios.post("/rider/clock_out")
-            .then(result => {
-                console.log(props);
-                const newClockInData = {...props.clockedInData};
-                newClockInData.clock_out = result.data.clock_out;
-                props.setClockedInData(newClockInData);
-            }).catch(error => {
-                console.log(error);
-            });
-    }
-    
+    const clockInTime = !props.clockedInData.clock_in
+        ? "" 
+        : moment(props.clockedInData.clock_in, "HH:mm").format("HH:mm a");
+
+    const clockOutTime = !props.clockedInData.clock_out
+        ? "" 
+        : moment(props.clockedInData.clock_out, "HH:mm").format("HH:mm a");
+   
+
     return (
         <div>
             <div className="mb-3">
-                <Button className="clock-in-out-button" onClick={clockIn} disabled={!!props.clockedInData.clock_in}>
+                <Button
+                    className="clock-in-out-button"
+                    onClick={() => showClockIn(true)}
+                    disabled={!!props.clockedInData.clock_in}>
                     Clock In
                 </Button>            
-                <span><b>Time: </b>{props.clockedInData.clock_in}</span>
+                <span><b>Time: </b>{clockInTime}</span>
             </div>
             
             <div>
-                <Button className="clock-in-out-button" onClick={clockOut} disabled={!!props.clockedInData.clock_out}>
+                <Button
+                    className="clock-in-out-button"
+                    onClick={() => showClockOut(true)}
+                    disabled={!!props.clockedInData.clock_out}>
                     Clock Out
                 </Button>
-                <span><b>Time: </b>{props.clockedInData.clock_out}</span>
+                <span><b>Time: </b>{clockOutTime}</span>
             </div>
+
+            <ClockIn show={isShowClockIn} onClose={() => showClockIn(false)} {...props}/>
+            <ClockOut show={isShowClockOut} onClose={() => showClockOut(false)} {...props}/>
         </div>      
     )
 };
--- db/migrate/20200330055914_create_working_intervals.rb
+++ db/migrate/20200330055914_create_working_intervals.rb
@@ -2,27 +2,29 @@
 
 class CreateWorkingIntervals < ActiveRecord::Migration[6.0]
   def up
-    # Create an auto-incremented id for Working Interval
-    execute "CREATE SEQUENCE wi_id_seq
+    execute <<-SQL
+      CREATE TYPE day_of_week AS ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+
+      CREATE SEQUENCE wi_id_seq
       START WITH 1
       INCREMENT BY 1
       NO MINVALUE
       NO MAXVALUE
-      CACHE 1;"
-    
-    execute "CREATE TABLE working_intervals (
-      wi_id bigint NOT NULL DEFAULT nextval('wi_id_seq'),
-      workingDay varchar(300) NOT NULL,
-      startHour varchar(300) NOT NULL,
-      endHour varchar(300) NOT NULL,
-      wws_id bigint NOT NULL,
-      PRIMARY KEY(wi_id),
-      FOREIGN KEY(wws_id) REFERENCES weekly_work_schedules
-          ON DELETE CASCADE
-    );"
+      CACHE 1;
 
-    # This will auto drop the sequence when Working Interval is dropped
-    execute "ALTER SEQUENCE wi_id_seq OWNED BY working_intervals.wi_id;"
+      CREATE TABLE working_intervals (
+        wi_id bigint NOT NULL DEFAULT nextval('wi_id_seq'),
+        workingDay day_of_week NOT NULL,
+        startHour time NOT NULL,
+        endHour time NOT NULL,
+        wws_id bigint NOT NULL,
+        PRIMARY KEY(wi_id),
+        FOREIGN KEY(wws_id) REFERENCES weekly_work_schedules
+            ON DELETE CASCADE
+      );
+
+      ALTER SEQUENCE wi_id_seq OWNED BY working_intervals.wi_id;
+    SQL
   end
 
   # For rolling back

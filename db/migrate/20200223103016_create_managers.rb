de2ec4c204efedca98319c22fca54b2cb9d0649a
Zhong Jun <zhongjun@jorked.com>
2020-04-07 21:52:54 +0800

    Convert to using SQL

--- db/migrate/20200223103016_create_managers.rb
+++ db/migrate/20200223103016_create_managers.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
 class CreateManagers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :managers do |t|
-      t.belongs_to :user, index: { unique: true }, foreign_key: "id"
+  def up
+    execute "CREATE SEQUENCE managers_id_seq
+      START WITH 1
+      INCREMENT BY 1
+      NO MINVALUE
+      NO MAXVALUE
+      CACHE 1;"
 
-      t.timestamps
-    end
+    execute "CREATE TABLE managers (
+      id bigint NOT NULL DEFAULT nextval('managers_id_seq'),
+      user_id bigint,
+      created_at timestamp(6) without time zone NOT NULL,
+      updated_at timestamp(6) without time zone NOT NULL,
+      PRIMARY KEY(id),
+      FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+    );"
+
+    execute "ALTER SEQUENCE managers_id_seq OWNED BY managers.id;"
+
+    execute "CREATE UNIQUE INDEX index_managers_on_user_id ON managers USING btree (user_id);"
+  end
+
+  def down
+    execute "DROP TABLE managers;"
+    execute "DROP INDEX index_managers_on_user_id;"
   end
 end

6045c1330ac3f57c806f6c4c5eeb32bfb8dd9b78
Chris Kheng <chriskheng1609@gmail.com>
2020-04-07 23:58:25 +0800

    Add constraints to working_intervals table

--- db/migrate/20200330055914_create_working_intervals.rb
+++ db/migrate/20200330055914_create_working_intervals.rb
@@ -15,11 +15,15 @@ class CreateWorkingIntervals < ActiveRecord::Migration[6.0]
       CREATE TABLE working_intervals (
         wi_id bigint NOT NULL PRIMARY KEY DEFAULT nextval('wi_id_seq'),
         workingDay day_of_week NOT NULL,
-        startHour time NOT NULL,
-        endHour time NOT NULL,
+        startHour time NOT NULL,                    
+        endHour time NOT NULL,                    
         wws_id bigint NOT NULL REFERENCES weekly_work_schedules
             ON DELETE CASCADE,
-        CONSTRAINT duration CHECK (endHour - startHour <= '4 hours') 
+        CONSTRAINT duration CHECK (endHour - startHour <= '4 hours'),
+        CONSTRAINT start_and_end_on_hour
+          CHECK ((EXTRACT(MINUTE FROM startHour) = 0) AND (EXTRACT(MINUTE FROM endHour) = 0)),
+        CONSTRAINT end_hour CHECK (endhour > startHour),
+        CONSTRAINT operating_hours CHECK (startHour >= time '10:00' AND endHour <= time '22:00')
       );
 
       ALTER SEQUENCE wi_id_seq OWNED BY working_intervals.wi_id;
@@ -27,7 +31,10 @@ class CreateWorkingIntervals < ActiveRecord::Migration[6.0]
   end
 
   # For rolling back
+  # Need to drop the sequence too
   def down
-    execute "DROP TABLE working_intervals;"
+    execute <<-SQL
+      DROP TABLE working_intervals;
+    SQL
   end
 end

a915820b3f63b5cc5e11eb22ba96d6e15efee9d2
Chris Kheng <chriskheng1609@gmail.com>
2020-04-09 21:31:20 +0800

    Disable core services when it's not operating hours (#161)

--- app/javascript/components/customers/CustomerNavBar.js
+++ app/javascript/components/customers/CustomerNavBar.js
@@ -2,13 +2,18 @@ import React from 'react';
 import AppBar from '../global/AppBar';
 import Colors from '../global/Colors';
 import Nav from 'react-bootstrap/Nav';
+import { isNotOperatingHours } from '../helpers/Helpers';
 
 const CustomerNavBar = () => {
     const navLinkstyle = Colors.navbarLink;
 
+    const isOrderDisabled = isNotOperatingHours();
+
     return (
         <AppBar isLoggedIn={true}>
-            <Nav.Link href="/customer/order" style={navLinkstyle}>ORDER</Nav.Link>
+            <Nav.Link href="/customer/order" style={isOrderDisabled ? null : navLinkstyle} disabled={isOrderDisabled}>
+                ORDER
+            </Nav.Link>
             <Nav.Link href="/customer/cart" style={navLinkstyle}>CART</Nav.Link>
             <Nav.Link href="/customer/history" style={navLinkstyle}>History</Nav.Link>
             <Nav.Link href="/customer/reviews" style={navLinkstyle}>Reviews</Nav.Link>
